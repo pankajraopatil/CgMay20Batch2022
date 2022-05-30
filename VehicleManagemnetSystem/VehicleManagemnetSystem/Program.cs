1e9c4fb77564d461a163cb52b906e23721d92168
Pankaj Patil <pankajraopatil@gmail.com>
2022-05-30 11:27:47 +0530

    Commit Done by Pankaj

---
+++ VehicleManagemnetSystem/VehicleManagemnetSystem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleManagemnetSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sample Demo On GIT");
+        }
+    }
+}
