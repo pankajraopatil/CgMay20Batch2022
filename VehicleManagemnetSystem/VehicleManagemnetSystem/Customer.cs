10e3e2077ddf2e95ab23c5fbc4f33ae4eb54e8e0
Pankaj Patil <pankajraopatil@gmail.com>
2022-05-30 12:28:48 +0530

    New class Added

--- VehicleManagemnetSystem/VehicleManagemnetSystem/Customer.cs
+++ VehicleManagemnetSystem/VehicleManagemnetSystem/Customer.cs
@@ -1,10 +1,14 @@
 using System;
 namespace VehicleManagemnetSystem
 {
- public class Customer
- {
-        public int CustomerID{get;set;}
-        public string CustomerName{get;set;}
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
         public string Address { get; set; }
+    }
+    public class PrimeCustomer
+    {
+        public bool IsActive { get; set; }
     }
 }
