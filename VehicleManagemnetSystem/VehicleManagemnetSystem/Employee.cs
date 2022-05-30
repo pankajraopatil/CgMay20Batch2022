28e80aa72a36cec98147af72800e2df38d3d73b7
Pankaj Patil <pankajraopatil@gmail.com>
2022-05-30 11:30:33 +0530

    New Class Employee Added by Riya on Date 30/05/2022 @ 11.30

---
+++ VehicleManagemnetSystem/VehicleManagemnetSystem/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleManagemnetSystem
+{
+   public class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+    }
+}
